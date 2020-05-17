89a75cefd7f4d5b309c5a5dafe7db381fc9cc2fa
bit00 <akbrijesh@gmail.com>
2020-05-17 21:39:55 +0530

    initial commit

---
+++ lib/src/article.dart
@@ -0,0 +1,150 @@
+class Article{
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article(
+    {this.text,
+    this.domain,
+    this.by,
+    this.age,
+    this.score,
+    this.commentsCount,}
+  );
+}
+
+  final articles = [
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:" Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+    new Article(
+      text:"Circualr Shock Acoustic Waves in Ionosphere",
+      domain:"wiley.com",
+      by:"zdw",
+      age: "3 hours",
+      score : 177,
+      commentsCount: 64,
+    ),
+    new Article(
+      text:"Better Faster Safer Happier Cheaper",
+      domain:"smart.com",
+      by:"jonsmart",
+      age: "2 hours",
+      score : 17,
+      commentsCount: 264,
+    ),
+
+  ];
+
